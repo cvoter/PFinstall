e20aa8c20d8d57e03d12fbac69f623ae4f31695f
Carolyn Voter <cvoter@wisc.edu>
2016-06-20 16:42:01 -0500

    Updated HDF5 version to current one

--- setupParflow.sh
+++ setupParflow.sh
@@ -42,7 +42,7 @@ export PARFLOW_DIR=$BASE/parflow
 export SILO_PATH=$BASE/silo-4.9.1-bsd
 export HYPRE_PATH=$BASE/hypre-2.9.0b
 export TCL_PATH=$BASE/tcl-8.6.5
-export HDF5_PATH=$BASE/hdf5-1.8.14/hdf5
+export HDF5_PATH=$BASE/hdf5-1.8.17/hdf5
 export MPI_PATH=/mnt/gluster/chtc/mpich-3.1
 export LD_LIBRARY_PATH=$MPI_PATH/lib:$LD_LIBRARY_PATH
 export PATH=$MPI_PATH/bin:$PATH
@@ -53,7 +53,7 @@ export PATH=$MPI_PATH/bin:$PATH
 #tar xzf silo-4.9.1-bsd.tar.gz
 #tar xfz hypre-2.9.0b.tar.gz
 #tar xfz tcl8.6.5-src.tar.gz
-tar xzf hdf5-1.8.16.tar.gz
+tar xzf hdf5-1.8.17.tar.gz
 
 # -------------------------------------------
 # INSTALL HYPRE
@@ -66,8 +66,8 @@ tar xzf hdf5-1.8.16.tar.gz
 
 # -------------------------------------------
 # INSTALL HDF5
-cd hdf5-1.8.16
-./configure --prefix=$BASE/hdf5-1.8.16 --enable-fortran --enable-cxx /
+cd hdf5-1.8.17
+./configure --prefix=$BASE/hdf5-1.8.17 --enable-fortran --enable-cxx /
 --enable-static-exec  --enable-using-memchecker --with-gnu-ld >> $BASE/installation_logs/hdf5.out 2>&1 || exit 1
 make >> $BASE/installation_logs/hdf5.out 2>&1 || exit 1
 make install >> $BASE/installation_logs/hdf5.out 2>&1 || exit 1
